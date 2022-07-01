f64ec4fb1bbf834da57560a37627d4d543926848
jehuicha <huichajuane@gmail.com>
2022-06-30 23:48:28 -0300

    Add files via upload
    Segunda entrega del proyecto final

--- PreEntrega/js/main.js
+++ PreEntrega/js/main.js
@@ -1,7 +1,8 @@
 class Tarea{
-    constructor(nombre){
-        this.nombre = nombre;
-        this.estado = 0;
+    constructor(name,state, id){
+        this.nombre = name;
+        this.estado = state;
+        this.key = id;
     }
     setEstado(newEstado){
         this.estado = newEstado;
@@ -11,11 +12,26 @@ class Tarea{
 const tareas = [];
 let elemento;
 let ul = document.getElementById("porHacerList");
+let li = document.getElementById("li");
 let item;
 let inputBoton = document.getElementById("botonInput");
-inputBoton.addEventListener("click",agregarTarea);
 let inputTarea = document.getElementById("inputTarea");
+let index = 0;
+
+inputBoton.addEventListener("click",agregarTarea);
 inputTarea.addEventListener("keypress",enterInput);
+ul.addEventListener("click", eliminar);
+
+let clave;
+for(let i=0; i<localStorage.length; i++){
+    clave = localStorage.key(i);
+    tareas.push( JSON.parse(localStorage.getItem(clave)));
+    item = document.createElement("li");
+    item.innerHTML= tareas[i].nombre;
+    ul.append(item);
+}
+index = parseInt(clave,10) + 1;
+
 function agregarTarea() {
     cargarTarea();
 }
@@ -26,10 +42,16 @@ function enterInput(e){
 function cargarTarea(){
     elemento = inputTarea.value;
     if(elemento!=null && elemento.length>0){
+        index++;
         item = document.createElement("li");
-        tareas.push(new Tarea(elemento));
+        item.id = index.toString();
+        tareas.push(new Tarea(elemento, 0, index));
+        localStorage.setItem(index, JSON.stringify(tareas[tareas.length-1]))
         item.innerHTML= elemento;
         ul.append(item);
         inputTarea.value = "";
     }
+}
+function eliminar(e){
+    console.log("Texto clickeado",e.target.innerText);
 }
