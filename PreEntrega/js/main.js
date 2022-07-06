17a79910d58ee0422b72cbdb5fc767bdabb773f2
jehuicha <huichajuane@gmail.com>
2022-07-06 00:35:36 -0300

    Add files via upload
    correccion importar

--- PreEntrega/js/main.js
+++ PreEntrega/js/main.js
@@ -1,13 +1,3 @@
-class Tarea{
-    constructor(name,state, id){
-        this.nombre = name;
-        this.estado = state;
-        this.key = id;
-    }
-    setEstado(newEstado){
-        this.estado = newEstado;
-    }
-}
 const tareas = [];
 let elemento;
 let item;
