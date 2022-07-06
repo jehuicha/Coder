8a855067d78868473c7a15648186b991558be36a
jehuicha <huichajuane@gmail.com>
2022-07-06 00:18:39 -0300

    Add files via upload

---
+++ PreEntrega/js/charge.js
@@ -0,0 +1,17 @@
+let clave;
+let tareaCargada;
+let ul = document.getElementById("porHacerList");
+let inputBoton = document.getElementById("botonInput");
+let inputTarea = document.getElementById("inputTarea");
+
+inputBoton.addEventListener("click",cargarTarea);
+inputTarea.addEventListener("keypress",enterInput);
+ul.addEventListener("click", eliminarTarea);
+for(let i=localStorage.length-1; i>=0; i--){
+    clave = localStorage.key(i);
+    tareaCargada = JSON.parse(localStorage.getItem(clave));
+    item = document.createElement("li");
+    item.setAttribute("id",clave);
+    item.innerHTML= tareaCargada.nombre;
+    ul.append(item);
+}
--- PreEntrega/js/main.js
+++ PreEntrega/js/main.js
@@ -8,33 +8,20 @@ class Tarea{
         this.estado = newEstado;
     }
 }
-
 const tareas = [];
 let elemento;
-let ul = document.getElementById("porHacerList");
-let li = document.getElementById("li");
 let item;
-let inputBoton = document.getElementById("botonInput");
-let inputTarea = document.getElementById("inputTarea");
+
 let index = 0;
-
-inputBoton.addEventListener("click",agregarTarea);
-inputTarea.addEventListener("keypress",enterInput);
-ul.addEventListener("click", eliminar);
-
-let clave;
-for(let i=0; i<localStorage.length; i++){
+for(let i=localStorage.length-1; i>=0; i--){
     clave = localStorage.key(i);
-    tareas.push( JSON.parse(localStorage.getItem(clave)));
-    item = document.createElement("li");
-    item.innerHTML= tareas[i].nombre;
-    ul.append(item);
+    tareaCargada = JSON.parse(localStorage.getItem(clave));
+    tareas.push(tareaCargada);
 }
-index = parseInt(clave,10) + 1;
+console.log("tareas",tareas);
 
-function agregarTarea() {
-    cargarTarea();
-}
+index = localStorage.length>0 ? parseInt(clave,10) + 1 : 0;
+
 function enterInput(e){
     if(e.key == "Enter")
         cargarTarea();
@@ -42,16 +29,28 @@ function enterInput(e){
 function cargarTarea(){
     elemento = inputTarea.value;
     if(elemento!=null && elemento.length>0){
-        index++;
+        index = index +1;
         item = document.createElement("li");
-        item.id = index.toString();
         tareas.push(new Tarea(elemento, 0, index));
         localStorage.setItem(index, JSON.stringify(tareas[tareas.length-1]))
         item.innerHTML= elemento;
+        item.setAttribute("id",index);
         ul.append(item);
         inputTarea.value = "";
     }
 }
-function eliminar(e){
+function eliminarTarea(e){
     console.log("Texto clickeado",e.target.innerText);
+    console.log("id del texto clickeado",e.target.id,"       tareas.length",tareas.length);
+    for(let i = 0; i<tareas.length; i++){
+        console.log("for i:",i,"     tareas[i].key == e.target.id",tareas[i].key," == ",e.target.id,"\n-*-*-* tareas[i]",tareas[i]);
+        if(tareas[i].key == e.target.id){
+            console.log("if i:",i,"   id:",e.target.id);
+            let toRemove = document.getElementById(e.target.id);
+            localStorage.removeItem(e.target.id);
+            tareas.splice(i, 1);
+            toRemove.remove();
+            break;
+        }
+    }
 }
